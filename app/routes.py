fa2efb6fb09950ed98a09c136a48f35fc01168d9
Mario Aburto <marioaburto10@gmail.com>
2018-11-06 17:44:39 -0500

    uploading and streaming files into flask server by chunks into a temp file

--- app/routes.py
+++ app/routes.py
@@ -6,21 +6,21 @@ from app import app
 @app.route('/', methods=['GET', 'POST'])
 def index():
 	if request.method == 'POST':
+		with open("/tmp/output_file", "bw") as f:
+			chunk_size = 4096
+			while True:
+				chunk = request.stream.read(chunk_size)
+				# print(chunk)
+				if len(chunk) == 0:
+					break
+				f.write(chunk)
+		
+			with open("/tmp/output_file", "br") as f:
+				 	print(f.read())
 
-		if request.files.get('file'):
-			# read the file
-			file = request.files['file']
+		return jsonify("Successful upload")
 
-			# read the filename
-			filename = file.filename
-
-			# create a path to the uploads folder
-			# filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-
-			# Save the file to the uploads folder
-			# file.save(filepath)
-
-			return jsonify(filename)
+	# print("THIS IS WHAT F IS: ", f.name)
 
 	return render_template('index.html')
 
