e316577d341ef6bcbd989bf522c18fae7c58bc48
Mario Aburto <marioaburto10@gmail.com>
2018-11-06 13:20:52 -0500

    able to upload files to server

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,8 @@
 from flask import Flask
 
 app = Flask(__name__)
+# point to uploads folder incase we want to save the files we are uploading
+app.config['UPLOAD_FOLDER'] = 'uploads'
 
+# bring in the routes, currently only one index route
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,27 @@
+import os
+from flask import Flask, request, jsonify, render_template
 from app import app
 
-@app.route('/')
-@app.route('/index')
+# home route which is a get and post route
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return "Hello, World!"
+	if request.method == 'POST':
+
+		if request.files.get('file'):
+			# read the file
+			file = request.files['file']
+
+			# read the filename
+			filename = file.filename
+
+			# create a path to the uploads folder
+			# filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+
+			# Save the file to the uploads folder
+			# file.save(filepath)
+
+			return jsonify(filename)
+
+	return render_template('index.html')
+
 
